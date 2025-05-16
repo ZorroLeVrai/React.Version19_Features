d57bb67b29f4ce9188f275a802eecc3e1c67cf2e
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2025-05-16 14:05:33 +0200

    Adding the handling of an error state

--- src/useAction/ActionDemo.tsx
+++ src/useAction/ActionDemo.tsx
@@ -1,31 +1,53 @@
 import React, { useActionState } from 'react'
 
+interface FormState {
+  userName: string;
+  error: string | null;
+}
+
 const NonActionDemo = () => {
   // useActionState is used here to manage the state of the form submission
-  const [userName, formAction, isPending] = useActionState(actionReducer, "");
+  const [formState, formAction, isPending] = useActionState<FormState, FormData>(actionReducer, {
+    userName: "",
+    error: null
+  });
 
-  async function actionReducer(prevState: string, formData: FormData) {
+  async function actionReducer(prevState: FormState, formData: FormData) {
     try {
       const inputedUserName = formData.get("userName") as string
       const userName = await getUserName(inputedUserName);
-      return userName;
+      return { userName, error: null };
     }
-    catch (error) {
-      console.error("Error updating data:", error);
-      return prevState;
+    catch (error: unknown) {
+      console.error(error);
+      if (isString(error)) {
+        return { ...prevState, error };
+      }
+      return { ...prevState, error: "An unknown error occurred" };
+    }
+
+    //TypeScript-specific way to narrow down the type of error
+    function isString(value: unknown): value is string {
+      return typeof value === "string";
     }
   }
 
   async function getUserName(userName: string) {
-    return await new Promise<string>((resolve) => setTimeout(() => {
+    return await new Promise<string>((resolve, reject) => setTimeout(() => {
+      if (userName.toLowerCase() === "error") {
+        reject("Error: User name cannot be 'error'");
+        return;
+      }
+
       resolve(userName);
     }, 1000));
   }
 
   return (
     <>
+      {!isPending && formState.error && <p style={{ color: "red" }}>{formState.error}</p>}
       <p>
-        Current user: <span>{isPending ? "Loading..." : userName}</span>
+        Current user: <span>{isPending ? "Loading..." : formState.userName}</span>
       </p>
       <form action={formAction}>
         <input
